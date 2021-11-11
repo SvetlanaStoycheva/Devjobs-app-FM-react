12e52d6b65fbb55a56280b0a3dc55557f1aaeb91
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-11-11 16:56:39 +0200

    all companies are displayed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
 import Companies from './components/Companies';
+import { useGlobalContext } from './context';
 
 function App() {
+  const { theme } = useGlobalContext();
   return (
     <>
-      <Header />
-      <Search />
-      <Companies />
+      <main className={`${theme === 'light' ? 'light-theme' : 'dark-theme'}`}>
+        <Header />
+        <Search />
+        <Companies />
+      </main>
     </>
   );
 }
--- src/components/Companies.js
+++ src/components/Companies.js
@@ -5,35 +5,43 @@ import logo1 from '../';
 const Companies = () => {
   const { all_companies, theme } = useGlobalContext();
   return (
-    <section
-      className={`${
-        theme === 'light' ? 'companies light-theme' : 'companies dark-theme'
-      }`}
-    >
-      {all_companies.map((item) => {
-        const {
-          id,
-          company,
-          logo,
-          logoBackground,
-          position,
-          postedAt,
-          contract,
-          location,
-        } = item;
-        console.log(logo);
+    <section className='companies'>
+      <div className='all-companies-container'>
+        {all_companies.map((item) => {
+          const {
+            id,
+            company,
+            logo,
+            logoBackground,
+            position,
+            postedAt,
+            contract,
+            location,
+          } = item;
+          console.log(logo);
 
-        return (
-          <article className='single-company' key={id}>
-            <img
-              src={logo}
-              alt='company-logo'
-              style={{ background: logoBackground }}
-            />
-          </article>
-        );
-      })}
-      <button></button>
+          return (
+            // single company container
+            <article className='single-company' key={id}>
+              <img
+                src={logo}
+                alt='company-logo'
+                style={{ background: logoBackground }}
+                className='company-logo'
+              />
+              <div className='contract-container'>
+                <p>{postedAt}</p>
+                <p className='dot'>.</p>
+                <p>{contract}</p>
+              </div>
+              <h3 className='position'>{position}</h3>
+              <p className='company-name'>{company}</p>
+              <h4 className='company-location'>{location}</h4>
+            </article>
+          );
+        })}
+      </div>
+      <button className='submit-btn load-btn'>Load More</button>
     </section>
   );
 };
